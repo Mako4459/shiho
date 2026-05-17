f0f929562f67107f278491e1d49061444aa0a95f
Mako4459 <legacy_elt69@yahoo.co.jp>
2026-05-18 01:33:35 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME='shiho-cache-v1';
+self.addEventListener('install',e=>{
+ e.waitUntil(
+  caches.open(CACHE_NAME).then(c=>c.addAll([
+   './',
+   './index.html',
+   './style.css',
+   './app.js',
+   './data.js',
+   './theme.js'
+  ]))
+ );
+});
+self.addEventListener('fetch',e=>{
+ e.respondWith(
+  caches.match(e.request).then(r=>r||fetch(e.request))
+ );
+});
---
+++ theme.js
@@ -0,0 +1,5 @@
+
+function clearTheme(){
+  localStorage.removeItem("shihoTheme");
+  document.body.classList.remove("white-theme");
+}
